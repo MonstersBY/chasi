9da93c5da4f8ddfb2cc67ff2f44a59a35409a2a0
Алина Рыскова <teekupinmuotti@gmail.com>
2023-10-30 18:01:11 +0300

    start adv page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./index.scss";
 import "./js/pages/home";
+import "./js/pages/adv-card";
 import "./js/components/header";
 import { example } from "./js/utils/constants";
 console.log(example);
