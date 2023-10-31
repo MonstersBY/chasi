b207c1b3cb4fad042d481a6e7931a4ec892e3817
MonstersBY <monsters.alex0@gmail.com>
2023-10-31 17:06:21 +0300

    fix: webpack

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "swiper/css/mousewheel";
 
 import "./index.scss";
 import "./js/pages/home_page";
-import "./js/pages/home";
 import "./js/pages/adv-card";
 import "./js/components/header";
 import { example } from "./js/utils/constants";
