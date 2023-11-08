c7ebbd3f89143ea4fffad10e3a44f4d12ab4a2a6
Aleftina17 <teekupinmuotti@gmail.com>
2023-11-08 13:14:54 +0300

    fix: creating-adv_page

--- src/js/pages/creating_adv_page.js
+++ src/js/pages/creating_adv_page.js
@@ -1,49 +1,47 @@
 import $ from "jquery";
-
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+import Swiper from "swiper";
+import "swiper/css";
 
 //sections switcher
 
-$(function() {
-  let $cardsContainer = $('.creating-adv__cards');
-  let $cards = $cardsContainer.find('.creating-adv__card');
-  
-  $cards.hide();
-  $cards.filter('.card--dafts').show();
+$(function () {
+  const $switchers = $(".switcher");
+  const $cards = $(".creating-adv__card");
 
-  $('.switcher').on('click', function(evt) {
-    evt.preventDefault();
-    let $this = $(this);
-    let classToFilter = $this.attr('class').split('--')[1];
+  $switchers.on("click", function (evt) {
+      evt.preventDefault();
+      const $this = $(this);
+      const classToFilter = $this.attr("class").split("--")[1];
+      const $parent = $this.parent();
 
-    if (!$this.hasClass('switcher--active')) {
-      $this.siblings('.switcher').removeClass('switcher--active');
-      $this.addClass('switcher--active');
- 
-      $cards.hide();
-      $cards.filter('.card--' + classToFilter).show();
-    }
+      if (!$parent.hasClass("creating-adv__section--active")) {
+          $switchers.parent().removeClass("creating-adv__section--active");
+          $parent.addClass("creating-adv__section--active");
+
+          $cards.hide();
+          $cards.filter(".card--" + classToFilter).show();
+      }
   });
+});
+
+//sections swiper on mobile
+
+const swiper = new Swiper(".switcherSwiper", {
+    slidesPerView: 3,
 });
 
 //if no cards show .creating-adv__empty
 
-$(function() {
-  let $cardContainer = $('.creating-adv__cards');
-  let $emptyBlock = $('.creating-adv__empty');
+$(function () {
+    let $cardContainer = $(".creating-adv__cards");
+    let $emptyBlock = $(".creating-adv__empty");
 
-  if ($cardContainer.find('.creating-adv__card').length === 0) {
-      $('.creating-adv__header, .creating-adv__sections, .creating-adv__cards').hide();
-      $emptyBlock.show();
-  } else {
-      $emptyBlock.hide();
-  }
+    if ($cardContainer.find(".creating-adv__card").length === 0) {
+        $(
+            ".creating-adv__header, .creating-adv__sections, .creating-adv__cards"
+        ).hide();
+        $emptyBlock.show();
+    } else {
+        $emptyBlock.hide();
+    }
 });
-
-
-
-
-
-
-
