76609f648f3c0958d0aeee567a4d71cfb539c8c5
MonstersBY <monsters.alex0@gmail.com>
2023-11-08 18:09:16 +0300

    feat: seller card

---
+++ src/js/components/modal.js
@@ -0,0 +1,37 @@
+import $ from "jquery";
+$(document).ready(function () {
+
+	$(document).on('keyup', (evt) => {
+		evt.keyCode === 27 ? closeModal() : null;
+	})
+
+	$('[data-modal="seller-card"]').on('click', () => {
+		$('.seller-card-modal').addClass('active');
+	});
+
+	$('[data-modal]').on('click', () => {
+		$('body').addClass('lock');
+	})
+
+	$('.modal-back').on('click', closeModal);
+	$('.modal-return').on('click', closeModal);
+	$('.modal-exit').on('click', closeModal);
+
+	function closeModal() {
+		$('.modal').removeClass('active');
+		$('body').removeClass('lock');
+	}
+
+	$('.seller-card-modal__form').on('submit', function(evt) {
+		evt.preventDefault();
+		closeModal()
+		$('.pop-up').addClass('showed')
+		setTimeout(()=> {
+			$('.pop-up').removeClass('showed')
+		}, 5000);
+	});
+
+	$('.pop-up').find('svg').on('click', () => {
+		$('.pop-up').removeClass('showed')
+	})
+})
