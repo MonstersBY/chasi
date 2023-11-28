c74147cc3758d420a4919d9a90fe679791434431
Aleftina17 <teekupinmuotti@gmail.com>
2023-11-28 15:54:02 +0300

    fix: chat page

--- src/js/pages/chat_page.js
+++ src/js/pages/chat_page.js
@@ -2,41 +2,84 @@ import $ from "jquery";
 
 //Show chat on dialog click
 
-$(function() {
-  const chat = $(".chat__dialog");
-  const dialogsItems = $(".dialogs__item");
-  const dialogsList = $(".chat__dialogs");
-  const btnBack = $("#btnBackToDialogs");
-
-  dialogsItems.on("click", function () {
+$(function () {
     if (window.innerWidth < 769) {
-      chat.css("display", "flex");
-      dialogsList.hide();
-    } else {
-      chat.css("display", "flex");
-      dialogsList.css("display", "flex");
-    }
-  });
+        $(".chat__switcher-content").hide();
+        $('.chat__switcher-content[data-content="dialogSupport"]').show();
+        handleMessageBtn();
 
-  btnBack.on("click", function () {
-    chat.css("display", "none");
-    dialogsList.css("display", "flex");
-  });
+        $(".chat__dialogs__item").on("click", function () {
+            $(".chat__switcher-content").hide();
+            let switcherValue = $(this).data("switcher");
+            $('.chat__switcher-content[data-content="' + switcherValue + '"]').show();
+            $(".chat__dialog").css("right", "0%");
+            handleMessageBtn();
+        });
+
+        $(".btn--close-dialog").on("click", function () {
+            $(".chat__dialog").css("right", "-100%");
+        });
+    } else {
+        $(".chat__switcher-content").hide();
+        $('.chat__switcher-content[data-content="dialogSupport"]').show();
+        handleMessageBtn();
+
+        $(".chat__dialogs__item").on("click", function () {
+            $(".chat__switcher-content").hide();
+            let switcherValue = $(this).data("switcher");
+            $('.chat__switcher-content[data-content="' + switcherValue + '"]').show();
+            handleMessageBtn();
+        });
+    }
 });
 
-//Send message button
+// $(function() {
+//   const chat = $(".chat__dialog");
+//   const dialogsItems = $(".dialogs__item");
+//   const dialogsList = $(".chat__dialogs");
+//   const btnBack = $("#btnBackToDialogs");
 
-$(function(){
-  const $messageTextarea = $('#messageTextarea')
-  const $sendButton = $('#sendButton')
+//   dialogsItems.on("click", function () {
+//     if (window.innerWidth < 769) {
+//       chat.css("display", "flex");
+//       dialogsList.hide();
+//     } else {
+//       chat.css("display", "flex");
+//       dialogsList.css("display", "flex");
+//     }
+//   });
 
-  $messageTextarea.on('input', function(){
-    if($messageTextarea.val().trim() !== ''){
-      $sendButton.show()
-    } else {
-      $sendButton.hide()
-    }
-  })
-})
+//   btnBack.on("click", function () {
+//     chat.css("display", "none");
+//     dialogsList.css("display", "flex");
+//   });
+// });
 
+//Dialogs switcher
+$(function () {
+    $(".chat__switcher-content").hide();
+    $('.chat__switcher-content[data-content="dialogSupport"]').show();
+    handleMessageBtn();
 
+    $(".chat__dialogs__item").on("click", function () {
+        $(".chat__switcher-content").hide();
+        let switcherValue = $(this).data("switcher");
+        $('.chat__switcher-content[data-content="' + switcherValue + '"]').show();
+        handleMessageBtn();
+    });
+});
+
+//Show/hise send message btn
+const handleMessageBtn = function () {
+    const $dialog = $(".chat__switcher-content:visible");
+    const $messageTextarea = $dialog.find(".chat__dialog__textarea");
+    const $sendButton = $dialog.find(".btn--send");
+    $sendButton.hide();
+    $messageTextarea.on("input", function () {
+        if ($messageTextarea.val().trim() !== "") {
+            $sendButton.show();
+        } else {
+            $sendButton.hide();
+        }
+    });
+};
